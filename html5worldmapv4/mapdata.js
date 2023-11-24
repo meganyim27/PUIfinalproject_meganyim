2069bb2c5ac10d154a3cd2ee2ece9d101d505b60
meganyim <meganyim@andrew.cmu.edu>
2023-11-24 15:10:39 -0800

    add map

---
+++ html5worldmapv4/mapdata.js
@@ -0,0 +1,1002 @@
+var simplemaps_worldmap_mapdata={
+  main_settings: {
+   //General settings
+    width: "1200", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    popups: "detect",
+    state_description: "",
+    state_color: "#7E9198",
+    state_hover_color: "#2D3842",
+    state_url: "",
+    border_size: 1,
+    border_color: "#ffffff",
+    all_states_inactive: "no",
+    all_states_zoomable: "no",
+    
+    //Location defaults
+    location_description: "click to see my trip!",
+    location_color: "#000000",
+    location_opacity: ".7",
+    location_hover_opacity: 1,
+    location_pulse: "yes",
+    location_url: "",
+    location_size: "18",
+    location_type: "circle",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#ffffff",
+    label_hover_color: "#ffffff",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: false,
+    manual_zoom: "yes",
+    back_image: "no",
+    arrow_box: "no",
+    navigation_size: "40",
+    navigation_color: "#f7f7f7",
+    navigation_border_color: "#636363",
+    initial_back: "no",
+    initial_zoom: -1,
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    rotate: "0",
+    url_new_tab: "yes",
+    images_directory: "default",
+    import_labels: "no",
+    fade_time: 0.1,
+    link_text: "View Website",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    AE: {
+      name: "United Arab Emirates",
+      inactive: "no",
+      hide: "no"
+    },
+    AF: {
+      name: "Afghanistan",
+      hide: "no",
+      inactive: "no"
+    },
+    AL: {
+      name: "Albania",
+      hide: "no",
+      inactive: "no"
+    },
+    AM: {
+      name: "Armenia",
+      hide: "no",
+      inactive: "no"
+    },
+    AO: {
+      name: "Angola",
+      hide: "no",
+      inactive: "no"
+    },
+    AR: {
+      name: "Argentina",
+      hide: "no",
+      inactive: "no"
+    },
+    AT: {
+      name: "Austria",
+      hide: "no",
+      inactive: "no"
+    },
+    AU: {
+      name: "Australia",
+      hide: "no",
+      inactive: "no"
+    },
+    AZ: {
+      name: "Azerbaidjan",
+      hide: "no",
+      inactive: "no"
+    },
+    BA: {
+      name: "Bosnia-Herzegovina",
+      hide: "no",
+      inactive: "no"
+    },
+    BD: {
+      name: "Bangladesh",
+      hide: "no",
+      inactive: "no"
+    },
+    BE: {
+      name: "Belgium",
+      hide: "no",
+      inactive: "no"
+    },
+    BF: {
+      name: "Burkina Faso",
+      hide: "no",
+      inactive: "no"
+    },
+    BG: {
+      name: "Bulgaria",
+      hide: "no",
+      inactive: "no"
+    },
+    BH: {
+      name: "Bahrain",
+      hide: "no",
+      inactive: "no"
+    },
+    BI: {
+      name: "Burundi",
+      hide: "no",
+      inactive: "no"
+    },
+    BJ: {
+      name: "Benin",
+      hide: "no",
+      inactive: "no"
+    },
+    BN: {
+      name: "Brunei Darussalam",
+      hide: "no",
+      inactive: "no"
+    },
+    BO: {
+      name: "Bolivia",
+      hide: "no",
+      inactive: "no"
+    },
+    BR: {
+      name: "Brazil",
+      hide: "no",
+      inactive: "no"
+    },
+    BS: {
+      name: "Bahamas",
+      hide: "no",
+      inactive: "no"
+    },
+    BT: {
+      name: "Bhutan",
+      hide: "no",
+      inactive: "no"
+    },
+    BW: {
+      name: "Botswana",
+      hide: "no",
+      inactive: "no"
+    },
+    BY: {
+      name: "Belarus",
+      hide: "no",
+      inactive: "no"
+    },
+    BZ: {
+      name: "Belize",
+      hide: "no",
+      inactive: "no"
+    },
+    CA: {
+      name: "Canada",
+      hide: "no",
+      inactive: "no"
+    },
+    CD: {
+      name: "Congo",
+      hide: "no",
+      inactive: "no"
+    },
+    CF: {
+      name: "Central African Republic",
+      hide: "no",
+      inactive: "no"
+    },
+    CG: {
+      name: "Congo",
+      hide: "no",
+      inactive: "no"
+    },
+    CH: {
+      name: "Switzerland",
+      hide: "no",
+      inactive: "no"
+    },
+    CI: {
+      name: "Ivory Coast",
+      hide: "no",
+      inactive: "no"
+    },
+    CL: {
+      name: "Chile",
+      hide: "no",
+      inactive: "no"
+    },
+    CM: {
+      name: "Cameroon",
+      hide: "no",
+      inactive: "no"
+    },
+    CN: {
+      name: "China",
+      hide: "no",
+      inactive: "no"
+    },
+    CO: {
+      name: "Colombia",
+      hide: "no",
+      inactive: "no"
+    },
+    CR: {
+      name: "Costa Rica",
+      hide: "no",
+      inactive: "no"
+    },
+    CU: {
+      name: "Cuba",
+      hide: "no",
+      inactive: "no"
+    },
+    CV: {
+      name: "Cape Verde",
+      hide: "no",
+      inactive: "no"
+    },
+    CY: {
+      name: "Cyprus",
+      hide: "no",
+      inactive: "no"
+    },
+    CZ: {
+      name: "Czech Republic",
+      hide: "no",
+      inactive: "no"
+    },
+    DE: {
+      name: "Germany",
+      hide: "no",
+      inactive: "no"
+    },
+    DJ: {
+      name: "Djibouti",
+      hide: "no",
+      inactive: "no"
+    },
+    DK: {
+      name: "Denmark",
+      hide: "no",
+      inactive: "no"
+    },
+    DO: {
+      name: "Dominican Republic",
+      hide: "no",
+      inactive: "no"
+    },
+    DZ: {
+      name: "Algeria",
+      hide: "no",
+      inactive: "no"
+    },
+    EC: {
+      name: "Ecuador",
+      hide: "no",
+      inactive: "no"
+    },
+    EE: {
+      name: "Estonia",
+      hide: "no",
+      inactive: "no"
+    },
+    EG: {
+      name: "Egypt",
+      hide: "no",
+      inactive: "no"
+    },
+    EH: {
+      name: "Western Sahara",
+      hide: "no",
+      inactive: "no"
+    },
+    ER: {
+      name: "Eritrea",
+      hide: "no",
+      inactive: "no"
+    },
+    ES: {
+      name: "Spain",
+      hide: "no",
+      inactive: "no"
+    },
+    ET: {
+      name: "Ethiopia",
+      hide: "no",
+      inactive: "no"
+    },
+    FI: {
+      name: "Finland",
+      hide: "no",
+      inactive: "no"
+    },
+    FJ: {
+      name: "Fiji",
+      hide: "no",
+      inactive: "no"
+    },
+    FK: {
+      name: "Falkland Islands",
+      hide: "no",
+      inactive: "no"
+    },
+    FR: {
+      name: "France",
+      hide: "no",
+      inactive: "no"
+    },
+    GA: {
+      name: "Gabon",
+      hide: "no",
+      inactive: "no"
+    },
+    GB: {
+      name: "Great Britain",
+      hide: "no",
+      inactive: "no"
+    },
+    GE: {
+      name: "Georgia",
+      hide: "no",
+      inactive: "no"
+    },
+    GF: {
+      name: "French Guyana",
+      hide: "no",
+      inactive: "no"
+    },
+    GH: {
+      name: "Ghana",
+      hide: "no",
+      inactive: "no"
+    },
+    GL: {
+      name: "Greenland",
+      hide: "no",
+      inactive: "no"
+    },
+    GM: {
+      name: "Gambia",
+      hide: "no",
+      inactive: "no"
+    },
+    GN: {
+      name: "Guinea",
+      hide: "no",
+      inactive: "no"
+    },
+    GQ: {
+      name: "Equatorial Guinea",
+      hide: "no",
+      inactive: "no"
+    },
+    GR: {
+      name: "Greece",
+      hide: "no",
+      inactive: "no"
+    },
+    GS: {
+      name: "S. Georgia & S. Sandwich Isls.",
+      hide: "no",
+      inactive: "no"
+    },
+    GT: {
+      name: "Guatemala",
+      hide: "no",
+      inactive: "no"
+    },
+    GW: {
+      name: "Guinea Bissau",
+      hide: "no",
+      inactive: "no"
+    },
+    GY: {
+      name: "Guyana",
+      hide: "no",
+      inactive: "no"
+    },
+    HN: {
+      name: "Honduras",
+      hide: "no",
+      inactive: "no"
+    },
+    HR: {
+      name: "Croatia",
+      hide: "no",
+      inactive: "no"
+    },
+    HT: {
+      name: "Haiti",
+      hide: "no",
+      inactive: "no"
+    },
+    HU: {
+      name: "Hungary",
+      hide: "no",
+      inactive: "no"
+    },
+    IC: {
+      name: "Canary Islands",
+      hide: "no",
+      inactive: "no"
+    },
+    ID: {
+      name: "Indonesia",
+      hide: "no",
+      inactive: "no"
+    },
+    IE: {
+      name: "Ireland",
+      hide: "no",
+      inactive: "no"
+    },
+    IL: {
+      name: "Israel",
+      hide: "no",
+      inactive: "no"
+    },
+    IN: {
+      name: "India",
+      hide: "no",
+      inactive: "no"
+    },
+    IQ: {
+      name: "Iraq",
+      hide: "no",
+      inactive: "no"
+    },
+    IR: {
+      name: "Iran",
+      hide: "no",
+      inactive: "no"
+    },
+    IS: {
+      name: "Iceland",
+      hide: "no",
+      inactive: "no"
+    },
+    IT: {
+      name: "Italy",
+      hide: "no",
+      inactive: "no"
+    },
+    JM: {
+      name: "Jamaica",
+      hide: "no",
+      inactive: "no"
+    },
+    JO: {
+      name: "Jordan",
+      hide: "no",
+      inactive: "no"
+    },
+    JP: {
+      name: "Japan",
+      hide: "no",
+      inactive: "no"
+    },
+    KE: {
+      name: "Kenya",
+      hide: "no",
+      inactive: "no"
+    },
+    KG: {
+      name: "Kyrgyzstan",
+      hide: "no",
+      inactive: "no"
+    },
+    KH: {
+      name: "Cambodia",
+      hide: "no",
+      inactive: "no"
+    },
+    KP: {
+      name: "North Korea",
+      hide: "no",
+      inactive: "no"
+    },
+    KR: {
+      name: "South Korea",
+      hide: "no",
+      inactive: "no"
+    },
+    KW: {
+      name: "Kuwait",
+      hide: "no",
+      inactive: "no"
+    },
+    KZ: {
+      name: "Kazakhstan",
+      hide: "no",
+      inactive: "no"
+    },
+    LA: {
+      name: "Laos",
+      hide: "no",
+      inactive: "no"
+    },
+    LK: {
+      name: "Sri Lanka",
+      hide: "no",
+      inactive: "no"
+    },
+    LR: {
+      name: "Liberia",
+      hide: "no",
+      inactive: "no"
+    },
+    LS: {
+      name: "Lesotho",
+      hide: "no",
+      inactive: "no"
+    },
+    LT: {
+      name: "Lithuania",
+      hide: "no",
+      inactive: "no"
+    },
+    LU: {
+      name: "Luxembourg",
+      hide: "no",
+      inactive: "no"
+    },
+    LV: {
+      name: "Latvia",
+      hide: "no",
+      inactive: "no"
+    },
+    LY: {
+      name: "Libya",
+      hide: "no",
+      inactive: "no"
+    },
+    MA: {
+      name: "Morocco",
+      hide: "no",
+      inactive: "no"
+    },
+    MD: {
+      name: "Moldavia",
+      hide: "no",
+      inactive: "no"
+    },
+    ME: {
+      name: "Montenegro",
+      hide: "no",
+      inactive: "no"
+    },
+    MG: {
+      name: "Madagascar",
+      hide: "no",
+      inactive: "no"
+    },
+    MK: {
+      name: "Macedonia",
+      hide: "no",
+      inactive: "no"
+    },
+    ML: {
+      name: "Mali",
+      hide: "no",
+      inactive: "no"
+    },
+    MM: {
+      name: "Myanmar",
+      hide: "no",
+      inactive: "no"
+    },
+    MN: {
+      name: "Mongolia",
+      hide: "no",
+      inactive: "no"
+    },
+    MR: {
+      name: "Mauritania",
+      hide: "no",
+      inactive: "no"
+    },
+    MW: {
+      name: "Malawi",
+      hide: "no",
+      inactive: "no"
+    },
+    MX: {
+      name: "Mexico",
+      hide: "no",
+      inactive: "no"
+    },
+    MY: {
+      name: "Malaysia",
+      hide: "no",
+      inactive: "no"
+    },
+    MZ: {
+      name: "Mozambique",
+      hide: "no",
+      inactive: "no"
+    },
+    NA: {
+      name: "Namibia",
+      hide: "no",
+      inactive: "no"
+    },
+    NC: {
+      name: "New Caledonia (French)",
+      hide: "no",
+      inactive: "no"
+    },
+    NE: {
+      name: "Niger",
+      hide: "no",
+      inactive: "no"
+    },
+    NG: {
+      name: "Nigeria",
+      hide: "no",
+      inactive: "no"
+    },
+    NI: {
+      name: "Nicaragua",
+      hide: "no",
+      inactive: "no"
+    },
+    NL: {
+      name: "Netherlands",
+      hide: "no",
+      inactive: "no"
+    },
+    NO: {
+      name: "Norway",
+      hide: "no",
+      inactive: "no"
+    },
+    NP: {
+      name: "Nepal",
+      hide: "no",
+      inactive: "no"
+    },
+    NZ: {
+      name: "New Zealand",
+      hide: "no",
+      inactive: "no"
+    },
+    OM: {
+      name: "Oman",
+      hide: "no",
+      inactive: "no"
+    },
+    PA: {
+      name: "Panama",
+      hide: "no",
+      inactive: "no"
+    },
+    PE: {
+      name: "Peru",
+      hide: "no",
+      inactive: "no"
+    },
+    PG: {
+      name: "Papua New Guinea",
+      hide: "no",
+      inactive: "no"
+    },
+    PH: {
+      name: "Philippines",
+      hide: "no",
+      inactive: "no"
+    },
+    PK: {
+      name: "Pakistan",
+      hide: "no",
+      inactive: "no"
+    },
+    PL: {
+      name: "Poland",
+      hide: "no",
+      inactive: "no"
+    },
+    PR: {
+      name: "Puerto Rico",
+      hide: "no",
+      inactive: "no"
+    },
+    PS: {
+      name: "Palestine",
+      hide: "no",
+      inactive: "no"
+    },
+    PT: {
+      name: "Portugal",
+      hide: "no",
+      inactive: "no"
+    },
+    PY: {
+      name: "Paraguay",
+      hide: "no",
+      inactive: "no"
+    },
+    QA: {
+      name: "Qatar",
+      hide: "no",
+      inactive: "no"
+    },
+    RO: {
+      name: "Romania",
+      hide: "no",
+      inactive: "no"
+    },
+    RS: {
+      name: "Serbia",
+      hide: "no",
+      inactive: "no"
+    },
+    RU: {
+      name: "Russia",
+      hide: "no",
+      inactive: "no"
+    },
+    RW: {
+      name: "Rwanda",
+      hide: "no",
+      inactive: "no"
+    },
+    SA: {
+      name: "Saudi Arabia",
+      hide: "no",
+      inactive: "no"
+    },
+    SB: {
+      name: "Solomon Islands",
+      hide: "no",
+      inactive: "no"
+    },
+    SD: {
+      name: "Sudan",
+      hide: "no",
+      inactive: "no"
+    },
+    SE: {
+      name: "Sweden",
+      hide: "no",
+      inactive: "no"
+    },
+    SI: {
+      name: "Slovenia",
+      hide: "no",
+      inactive: "no"
+    },
+    SK: {
+      name: "Slovak Republic",
+      hide: "no",
+      inactive: "no"
+    },
+    SL: {
+      name: "Sierra Leone",
+      hide: "no",
+      inactive: "no"
+    },
+    SN: {
+      name: "Senegal",
+      hide: "no",
+      inactive: "no"
+    },
+    SO: {
+      name: "Somalia",
+      hide: "no",
+      inactive: "no"
+    },
+    SR: {
+      name: "Suriname",
+      hide: "no",
+      inactive: "no"
+    },
+    SS: {
+      name: "South Sudan",
+      hide: "no",
+      inactive: "no"
+    },
+    SV: {
+      name: "El Salvador",
+      hide: "no",
+      inactive: "no"
+    },
+    SY: {
+      name: "Syria",
+      hide: "no",
+      inactive: "no"
+    },
+    SZ: {
+      name: "Swaziland",
+      hide: "no",
+      inactive: "no"
+    },
+    TD: {
+      name: "Chad",
+      hide: "no",
+      inactive: "no"
+    },
+    TG: {
+      name: "Togo",
+      hide: "no",
+      inactive: "no"
+    },
+    TH: {
+      name: "Thailand",
+      hide: "no",
+      inactive: "no"
+    },
+    TJ: {
+      name: "Tadjikistan",
+      hide: "no",
+      inactive: "no"
+    },
+    TL: {
+      name: "East Timor",
+      hide: "no",
+      inactive: "no"
+    },
+    TM: {
+      name: "Turkmenistan",
+      hide: "no",
+      inactive: "no"
+    },
+    TN: {
+      name: "Tunisia",
+      hide: "no",
+      inactive: "no"
+    },
+    TR: {
+      name: "Turkey",
+      hide: "no",
+      inactive: "no"
+    },
+    TT: {
+      name: "Trinidad and Tobago",
+      hide: "no",
+      inactive: "no"
+    },
+    TW: {
+      name: "Taiwan",
+      hide: "no",
+      inactive: "no"
+    },
+    TZ: {
+      name: "Tanzania",
+      hide: "no",
+      inactive: "no"
+    },
+    UA: {
+      name: "Ukraine",
+      hide: "no",
+      inactive: "no"
+    },
+    UG: {
+      name: "Uganda",
+      hide: "no",
+      inactive: "no"
+    },
+    US: {
+      name: "United States",
+      inactive: "no",
+      hide: "no"
+    },
+    UY: {
+      name: "Uruguay",
+      hide: "no",
+      inactive: "no"
+    },
+    UZ: {
+      name: "Uzbekistan",
+      hide: "no",
+      inactive: "no"
+    },
+    VE: {
+      name: "Venezuela",
+      hide: "no",
+      inactive: "no"
+    },
+    VN: {
+      name: "Vietnam",
+      hide: "no",
+      inactive: "no"
+    },
+    VU: {
+      name: "Vanuatu",
+      hide: "no",
+      inactive: "no"
+    },
+    YE: {
+      name: "Yemen",
+      hide: "no",
+      inactive: "no"
+    },
+    ZA: {
+      name: "South Africa",
+      hide: "no",
+      inactive: "no"
+    },
+    ZM: {
+      name: "Zambia",
+      hide: "no",
+      inactive: "no"
+    },
+    ZW: {
+      name: "Zimbabwe",
+      hide: "no",
+      inactive: "no"
+    }
+  },
+  locations: {
+    "0": {
+      lat: "37.5519",
+      lng: "126.9918",
+      name: "Seoul",
+      color: "#E68570",
+      url: "file:///Users/meganyim/Library/Mobile%20Documents/com~apple~CloudDocs/cmu/junior/PUI/github%20repository/PUIfinalproject_meganyim/korea%20page.html"
+      // url: "https://www.fusioncharts.com/blog/complete-guide-to-javascript-interactive-map/#What_Is_JavaScript_Interactive_Map"
+    },
+    "1": {
+      lat: "41.3874",
+      lng: "2.1686",
+      name: "Barcelona",
+      color: "#B6896F"
+    },
+    "2": {
+      name: "Amsterdam",
+      lat: "52.3676",
+      lng: "4.9041",
+      color: "#B6896F"
+    },
+    "3": {
+      name: "Philadelphia",
+      lat: "39.9526",
+      lng: "-75.1652",
+      color: "#728A60"
+    },
+    "4": {
+      lat: "36.2679",
+      lng: "-112.3535",
+      name: "Grand Canyon",
+      color: "#728A60"
+    },
+    "5": {
+      lat: "22.3193",
+      lng: "114.1694",
+      name: "Hong Kong",
+      color: "#E68570"
+    }
+  },
+  labels: {},
+  regions: {},
+  legend: {
+    entries: []
+  }
+};
--- script.js
+++
@@ -1,5 +0,0 @@
-$("figure").mouseleave(
-    function () {
-    $(this).removeClass("hover");
-    }
-    );
